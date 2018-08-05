929e3a7e6c6a6b25b177452d1a0e0792410a05ae
Alex <alexmoldovan24@gmail.com>
2018-08-05 17:48:52 +0100

    process test

--- main.py
+++ main.py
@@ -3,25 +3,30 @@
 import sys
 import time
 import scrollphathd as sphd
-
-
-sphd.set_brightness(1.0)
-
-if len(sys.argv) != 2:
-    print("\nusage: python simple-text-scroll-rotated.py \"message\" \npress CTRL-C to exit\n")
-    sys.exit(0)
+from multiprocessing import Process
 
 #The screen is rotated on my robot case.
 sphd.rotate(180)
+sphd.set_brightness(1.0)
 
+if len(sys.argv) != 2:
+	print("\nusage: python simple-text-scroll-rotated.py \"message\" \npress CTRL-C to exit\n")
+	sys.exit(0)
 
-sphd.write_string(sys.argv[1], 11)
+def scroll(text):
+	sphd.clear()
+	sphd.write_string(text, 11)
+	while True:
+		sphd.show()
+		sphd.scroll(1)
+		time.sleep(0.05)
 
-while True:
-    try:
-    	sphd.show()
-        sphd.scroll(1)
-        time.sleep(0.05)
-    except KeyboardInterrupt:
-        sphd.clear()
-sys.exit()
+if __name__ == '__main__':
+	#Start scroll process
+	scrollProcess = Process(target=scroll, args=sys.argv[1])
+	scrollProcess.start()
+
+	sleep(5)
+
+	scrollProcess.terminate()
+	scrollProcess.join()
